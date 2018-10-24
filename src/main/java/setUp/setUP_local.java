9d79a4727066038ca25aac908fde03e11ddf7513
A.Krasheninnikov <izrail_izrail@rambler.ru>
2018-10-24 12:31:29 +0300

    devAd - наладил запуск браузера setUP-local

--- src/main/java/setUp/setUP_local.java
+++ src/main/java/setUp/setUP_local.java
@@ -1,19 +1,23 @@
 package setUp;
 
-import com.codeborne.selenide.WebDriverRunner;
+
 import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.DriverManagerType;
+import org.junit.After;
+import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
 
-import static com.codeborne.selenide.Configuration.browser;
 
 public class setUP_local {
     public WebDriver driver;
-    @BeforeClass()
+    @BeforeSuite()
     public void setUp(){
 //        DesiredCapabilities caps = new DesiredCapabilities();
 //        caps.setBrowserName("chrome");
@@ -23,9 +27,9 @@ public class setUP_local {
 //            caps = DesiredCapabilities.chrome();
 
         System.out.println("Запуск Chrome браузера");
-        ChromeDriverManager.getInstance().setup();
+        ChromeDriverManager.getInstance(DriverManagerType.CHROME).setup();
         driver = new ChromeDriver();
-        WebDriverRunner.setWebDriver(driver);
+//        WebDriverRunner.setWebDriver(driver);
         driver.manage().window().maximize();
 
         System.out.println("");
@@ -33,7 +37,7 @@ public class setUP_local {
         System.out.println("");
 
     }
-    @AfterClass(description = "Закрываем драйвер")
+    @AfterSuite()
     public void tearDown() {
         System.out.println("закрываем драйвер");
                 driver.quit();
--- src/test/java/autorisations/autorisation.java
+++ src/test/java/autorisations/autorisation.java
@@ -1,13 +1,17 @@
 package autorisations;
 
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
+import setUp.setUP_local;
 
 import static com.codeborne.selenide.Selenide.sleep;
 
-public class autorisation {
+public class autorisation extends setUP_local {
     @Test(description = "пробный")
     public void A() {
+        System.out.println("слип");
         sleep(10000);
+        System.out.println("конец слипа");
 
     }
 }
