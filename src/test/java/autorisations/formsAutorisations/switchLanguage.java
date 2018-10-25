46aadd15722c406e9644c7ebba8ac0c1d1bbe17e
A.Krasheninnikov <izrail_izrail@rambler.ru>
2018-10-25 14:36:48 +0300

    C56488 - Смена языка - done

--- src/test/java/autorisations/formsAutorisations/switchLanguage.java
+++ src/test/java/autorisations/formsAutorisations/switchLanguage.java
@@ -7,6 +7,8 @@ import main.java.setUp.setUP_local;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
+import static com.codeborne.selenide.Condition.hidden;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byAttribute;
 import static com.codeborne.selenide.Selectors.byClassName;
@@ -25,50 +27,56 @@ public class switchLanguage extends setUP_local{
     public void switchLanguage() {
         System.out.println("определение всех переменых ");
         SelenideElement global = $(By.id("app"));
+
         System.out.println("определение кнопки смены языка");
         SelenideElement switchLanguage = global.$(byClassName("el-dropdown"));
+
+        System.out.println("нажали на кнопку смены языка");
         switchLanguage.hover().click();
-        System.out.println();
+
+        System.out.println("определили форму выбора языка");
         SelenideElement switchLanguageDropDown = $(byClassName("el-dropdown-menu--medium"));
-        System.out.println();
+
+        System.out.println("определили кнопки выбора языка РУС/Инг");
         SelenideElement languageRUS = switchLanguageDropDown.$(byText("Русский"));
         SelenideElement languageENG = switchLanguageDropDown.$(byText("English"));
+
+        System.out.println("изменили язык на Инг");
         languageENG.hover().click();
+
+        System.out.println("проверка на push-ENG");
+        SelenideElement checkPushENG = $(byAttribute("role","alert"));
+        checkPushENG.findElements(byText("switch language success"));
+        if (checkPushENG.is(visible)) {
+            assertTrue(checkPushENG.is(Condition.text("switch language success")));
+            System.out.println("элемент видимый");
+        }
+        else{
+            System.out.println(" -- элемент не найден -- ");
+            assertTrue(checkPushENG.is(Condition.not(visible)),"Элемент не найден");
+        }
+
+        System.out.println("оиждание скрытия push");
+        SelenideElement pushHidden = $(byAttribute("role","alert"));
+        pushHidden.waitUntil(hidden,10000);
+
+        System.out.println("нажали на кнопку смены языка");
         switchLanguage.hover().click();
+
+        System.out.println("изменили язык на РУС");
         languageRUS.hover().click();
 
-        //---
-//        SelenideElement login = global.$(Selectors.byAttribute("name","username"));
-//        System.out.println("проверяем что поле не скрыто");
-//        login.shouldNotBe(Condition.hidden);
-//        System.out.println("ввод логина");
-//        login.setValue("userA");
-//        System.out.println("определение поля пароля");
-//        SelenideElement password = global.$(Selectors.byAttribute("name","password"));
-//        System.out.println("проверяем что поле не скрыто");
-//        password.shouldNotBe(Condition.hidden);
-//        System.out.println("ввод пароля");
-//        password.setValue("1234567890");
-//        System.out.println("определение кнопки войти");
-//        SelenideElement enter = global.$(byText("Войти"));
-//        System.out.println("проверяем что поле не скрыто");
-//        enter.shouldNotBe(Condition.hidden);
-//        System.out.println("нажать кнопку вход");
-//        enter.hover().click();
-//        System.out.println("проверка авторизации - появился push с ошибкой ");
-//        SelenideElement checkPush = $(byAttribute("role","alert"));
-//        checkPush.findElements(byText("Ошибка авторизации"));
-//        System.out.println();
-//        if (checkPush.is(visible)) {
-//            assertTrue(checkPush.is(Condition.text("Ошибка авторизации")));
-//            System.out.println("элемент видимый");
-//        }
-//
-//        else{
-//            System.out.println(" -- элемент не найден -- ");
-//            assertTrue(checkPush.is(Condition.not(visible)),"Элемент не найден");
-//        }
-//
+        System.out.println("проверка на push-RUS");
+        SelenideElement checkPushRUS = $(byAttribute("role","alert"));
+        checkPushRUS.findElements(byText("успешное переключение языка"));
+        if (checkPushRUS.is(visible)) {
+            assertTrue(checkPushRUS.is(Condition.text("успешное переключение языка")));
+            System.out.println("элемент видимый");
+        }
+        else{
+            System.out.println(" -- элемент не найден -- ");
+            assertTrue(checkPushRUS.is(Condition.not(visible)),"Элемент не найден");
+        }
         System.out.println("конец теста");
     }
 
