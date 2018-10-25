2860c8049967e65bb3373511c874fa5656d99d3a
A.Krasheninnikov <izrail_izrail@rambler.ru>
2018-10-25 11:38:45 +0300

    C56916 - авторизация положительная - done

--- src/test/java/autorisations/formsAutorisations/passAutorisation.java
+++ src/test/java/autorisations/formsAutorisations/passAutorisation.java
@@ -8,6 +8,11 @@ import main.java.setUp.setUP_local;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
+import static com.codeborne.selenide.Condition.hidden;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byClassName;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.sleep;
 import static org.testng.Assert.assertTrue;
 
@@ -22,14 +27,35 @@ public class passAutorisation extends setUP_local{
     public void autorizations() {
         System.out.println("определение всех переменых ");
         SelenideElement global = Selenide.$(By.id("app"));
-        System.out.println("ввод логина");
+        System.out.println("определение поля имя пользователя");
         SelenideElement login = global.$(Selectors.byAttribute("name","username"));
+        System.out.println("проверяем что поле не скрыто");
         login.shouldNotBe(Condition.hidden);
+        System.out.println("ввод логина");
         login.setValue("user");
-        sleep(7);
+        System.out.println("определение поля пароля");
+        SelenideElement password = global.$(Selectors.byAttribute("name","password"));
+        System.out.println("проверяем что поле не скрыто");
+        password.shouldNotBe(Condition.hidden);
         System.out.println("ввод пароля");
+        password.setValue("12345678");
+        System.out.println("определение кнопки войти");
+        SelenideElement enter = global.$(byText("Войти"));
+        System.out.println("проверяем что поле не скрыто");
+        enter.shouldNotBe(Condition.hidden);
         System.out.println("нажать кнопку вход");
-        System.out.println("проверка авторизации");
+        enter.hover().click();
+        System.out.println("проверка авторизации - открылась нужная страница");
+        SelenideElement checkOpen = global.$(byClassName("menu-wrapper")).$(By.tagName("a"));
+        checkOpen.findElements(byClassName("router-link-active"));
+        if (checkOpen.is(visible)) {
+            System.out.println("элемент видимый");
+        }
 
+        else{
+            System.out.println(" -- элемент не найден -- ");
+            assertTrue(checkOpen.is(Condition.not(visible)),"Элемент не найден");
+        }
+        System.out.println("конец теста");
     }
 }
