3a83bbb5287e72741ad83f455ac2e927e654d518
A.Krasheninnikov <izrail_izrail@rambler.ru>
2018-11-01 12:13:41 +0300

    C56961 - кодеки - done

---
+++ src/test/java/leftPanel/menuSystema/codecs.java
@@ -0,0 +1,101 @@
+package test.java.leftPanel.menuSystema;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.SelenideElement;
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+import main.java.setUp.setUP_local;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Condition.disappear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.getElement;
+import static org.openqa.selenium.By.tagName;
+import static org.testng.Assert.assertTrue;
+
+public class codecs extends setUP_local {
+
+    @Test(description = "проверка титла перед авторизацией")
+    public void title() {
+        System.out.println("проверка титла");
+        assertTrue(driver.getTitle().contains("Largo Admin 3.7.34"));
+        System.out.println("проверка фин");
+    }
+    @Test(description = "проверка титла перед авторизацией")
+    public void autorizations() {
+        System.out.println("определение всех переменых ");
+        SelenideElement global = $(By.id("app"));
+        System.out.println("определение поля имя пользователя");
+        SelenideElement login = global.$(Selectors.byAttribute("name","username"));
+        System.out.println("проверяем что поле не скрыто");
+        login.shouldNotBe(Condition.hidden);
+        System.out.println("ввод логина");
+        login.setValue("user");
+        System.out.println("определение поля пароля");
+        SelenideElement password = global.$(Selectors.byAttribute("name","password"));
+        System.out.println("проверяем что поле не скрыто");
+        password.shouldNotBe(Condition.hidden);
+        System.out.println("ввод пароля");
+        password.setValue("12345678");
+        System.out.println("определение кнопки войти");
+        SelenideElement enter = global.$(byText("Войти"));
+        System.out.println("проверяем что поле не скрыто");
+        enter.shouldNotBe(Condition.hidden);
+        System.out.println("нажать кнопку вход");
+        enter.hover().click();
+        System.out.println("проверка авторизации - открылась нужная страница");
+        SelenideElement checkOpen = global.$(byClassName("menu-wrapper")).$(tagName("a"));
+        checkOpen.findElements(byClassName("router-link-active"));
+        if (checkOpen.is(visible)) {
+            System.out.println("элемент видимый");
+        }
+
+        else{
+            System.out.println(" -- элемент не найден -- ");
+            assertTrue(checkOpen.is(Condition.not(visible)),"Элемент не найден");
+        }
+        System.out.println("конец теста");
+    }
+    @Test(description = "левая панель сворачиваем")
+    public void codecs() {
+        System.out.println("определение переменных");
+        SelenideElement global = $(byId("app"));
+        SelenideElement globalCentral = global.$(byClassName("main-container"));
+        SelenideElement globalTabs = globalCentral.$(byClassName("is-top"));
+        SelenideElement tabContainer = global.$(byClassName("tab-container"));
+        globalTabs.is(visible);
+
+        System.out.println("переключаемся на Кодеки");
+        ElementsCollection collSystemTabs = globalTabs.$(byClassName("el-tabs__nav")).$$(tagName("div"));
+        collSystemTabs.get(4).hover().shouldBe(text("Кодеки"));
+        collSystemTabs.get(4).hover().click();
+
+        System.out.println("определение переменных - внутри вкладки Кодеки");
+        SelenideElement top = tabContainer.$(byClassName("el-tabs__content"));
+        ElementsCollection row = top.$$(byClassName("is-scrolling-none"));
+        row.get(0).shouldBe(visible).getText();
+        assertTrue(row.get(0).is(visible));
+        System.out.println(row);
+
+        System.out.println("кнопочки настройки аудио / видео кодеков");
+        System.out.println("собираем кнопочки");
+        ElementsCollection but1 = global.$(byClassName("el-tabs__content")).$$(byAttribute("type","button")).filterBy(text("Настройка аудио кодеков"));
+        but1.get(0).hover();
+        assertTrue(but1.get(0).is(visible));
+        System.out.println(but1);
+
+        System.out.println("собираем кнопочки");
+        ElementsCollection but2 = global.$(byClassName("el-tabs__content")).$$(byAttribute("type","button")).filterBy(text("Настройка видео кодеков"));
+        but2.get(0).hover();
+        assertTrue(but2.get(0).is(visible));
+        System.out.println(but2);
+
+        System.out.println("конец теста");
+    }
+
+}
