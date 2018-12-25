42a8dd693c5926a21c7cee4b118e96b042d4a26b
A.Krasheninnikov <izrail_izrail@rambler.ru>
2018-12-25 18:33:10 +0300

    исправления по ошибкам

--- src/test/java/leftPanel/menuSystema/stateSystema/stateSystema.java
+++ src/test/java/leftPanel/menuSystema/stateSystema/stateSystema.java
@@ -546,10 +546,10 @@ public class stateSystema extends setUP_local{
         SelenideElement textInfoAllMemory = collSystemForms.get(7).shouldBe(text("kB"));
         SelenideElement textInfoClearMemory = collSystemForms.get(9).shouldBe(text("kB"));
         SelenideElement textInfoMacAdres = collSystemForms.get(11).shouldBe(text("e0:d5:5e:2e:05:38"));
-        SelenideElement textInfoVersionsProg = collSystemForms.get(13).shouldBe(text("2.6~176"));
+        SelenideElement textInfoVersionsProg = collSystemForms.get(13).shouldBe(text("2.6~244"));
         SelenideElement textInfoStatusActiv = collSystemForms.get(15).shouldBe(text("Активирован"));
         SelenideElement textInfoCapability = collSystemForms.get(17).shouldBe(text("4K, MCU"));
-        SelenideElement textInfoVersionsAPI = collSystemForms.get(19).shouldBe(text("1.3.14"));
+        SelenideElement textInfoVersionsAPI = collSystemForms.get(19).shouldBe(text("1.3.20"));
 
         textInfoCoreSystem.getText();
         textInfoModelProcessor.getText();
