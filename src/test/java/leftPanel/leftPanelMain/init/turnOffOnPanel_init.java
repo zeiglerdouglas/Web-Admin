286bfb0022d321cbd05f40e4536dd858eed9d075
A.Krasheninnikov <izrail_izrail@rambler.ru>
2019-02-19 12:37:30 +0300

    добавление id для тестов = 56492

--- src/test/java/leftPanel/leftPanelMain/init/turnOffOnPanel_init.java
+++ src/test/java/leftPanel/leftPanelMain/init/turnOffOnPanel_init.java
@@ -1,14 +1,15 @@
 package test.java.leftPanel.leftPanelMain.init;
 
+import main.java.annotations.TestRailId;
 import main.java.setUp.setUP_local;
 import org.testng.annotations.Test;
-import test.java.leftPanel.leftPanelMain.test.tabsPanel_test;
 import test.java.leftPanel.leftPanelMain.test.turnOffOnPanel_test;
 
 
 public class turnOffOnPanel_init extends setUP_local {
 
-    @Test(description = "проверка титла перед авторизацией и авторизация")
+    @Test(description = "сворачивание и разворачивание панели", groups = "leftPanel")
+    @TestRailId(id = "56492",version = "1")
     public void turnOffOnPanel () {
         System.out.println("проверка титла");
         turnOffOnPanel_test.turnOffOnPanel_title();
