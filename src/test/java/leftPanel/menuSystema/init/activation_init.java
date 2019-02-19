fed5334de1dc30a6d14a9c0ab123f400fae53a9d
A.Krasheninnikov <izrail_izrail@rambler.ru>
2019-02-19 13:12:03 +0300

    добавление id для тестов = 60008

--- src/test/java/leftPanel/menuSystema/init/activation_init.java
+++ src/test/java/leftPanel/menuSystema/init/activation_init.java
@@ -1,14 +1,15 @@
 package test.java.leftPanel.menuSystema.init;
 
+import main.java.annotations.TestRailId;
 import main.java.setUp.setUP_local;
 import org.testng.annotations.Test;
 import test.java.leftPanel.menuSystema.test.activation_test;
-import test.java.leftPanel.menuSystema.test.calls_test;
 
 
 public class activation_init extends setUP_local {
 
-    @Test(description = "проверка вкадки сеть ")
+    @Test(description = "присутствие кнопки активация",groups = "menuSystem")
+    @TestRailId(id = "60008",version = "1")
     public void activation() {
         System.out.println("проверка титла");
         activation_test.activation_title();
