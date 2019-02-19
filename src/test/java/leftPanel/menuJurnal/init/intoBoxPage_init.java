709afc72295c4b36402a50dccb1cd55c41f2ddb2
A.Krasheninnikov <izrail_izrail@rambler.ru>
2019-02-19 12:51:11 +0300

    добавление id для тестов = 56852

--- src/test/java/leftPanel/menuJurnal/init/intoBoxPage_init.java
+++ src/test/java/leftPanel/menuJurnal/init/intoBoxPage_init.java
@@ -1,14 +1,15 @@
 package test.java.leftPanel.menuJurnal.init;
 
+import main.java.annotations.TestRailId;
 import main.java.setUp.setUP_local;
 import org.testng.annotations.Test;
-import test.java.leftPanel.menuJurnal.test.clickPage_test;
 import test.java.leftPanel.menuJurnal.test.intoBoxPage_test;
 
 
 public class intoBoxPage_init extends setUP_local {
 
-    @Test(description = "проверка вкадки сеть ")
+    @Test(description = "переключение по страницам", groups = "menuJurnal")
+    @TestRailId(id = "56852",version = "1")
     public void intoBoxPage() {
         System.out.println("проверка титла");
         intoBoxPage_test.intoBoxPage_title();
