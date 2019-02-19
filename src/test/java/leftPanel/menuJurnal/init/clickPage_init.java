b25cb2805c2007646af680b0559d6952163e72af
A.Krasheninnikov <izrail_izrail@rambler.ru>
2019-02-19 12:55:01 +0300

    добавление id для тестов = 56853

--- src/test/java/leftPanel/menuJurnal/init/clickPage_init.java
+++ src/test/java/leftPanel/menuJurnal/init/clickPage_init.java
@@ -1,14 +1,15 @@
 package test.java.leftPanel.menuJurnal.init;
 
+import main.java.annotations.TestRailId;
 import main.java.setUp.setUP_local;
 import org.testng.annotations.Test;
 import test.java.leftPanel.menuJurnal.test.clickPage_test;
-import test.java.leftPanel.menuJurnal.test.switchPage_test;
 
 
 public class clickPage_init extends setUP_local {
 
-    @Test(description = "проверка вкадки сеть ")
+    @Test(description = "переходы на страницу ", groups = "menuJurnal")
+    @TestRailId(id = "56853",version = "1")
     public void clickPage() {
         System.out.println("проверка титла");
         clickPage_test.clickPage_title();
