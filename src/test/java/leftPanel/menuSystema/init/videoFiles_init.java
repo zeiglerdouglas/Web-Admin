50471000577c08698c109d1e1c8aead0ede8cf61
A.Krasheninnikov <izrail_izrail@rambler.ru>
2019-02-19 13:06:03 +0300

    добавление id для тестов = 56838

--- src/test/java/leftPanel/menuSystema/init/videoFiles_init.java
+++ src/test/java/leftPanel/menuSystema/init/videoFiles_init.java
@@ -1,14 +1,15 @@
 package test.java.leftPanel.menuSystema.init;
 
+import main.java.annotations.TestRailId;
 import main.java.setUp.setUP_local;
 import org.testng.annotations.Test;
-import test.java.leftPanel.menuSystema.test.calls_test;
 import test.java.leftPanel.menuSystema.test.videoFiles_test;
 
 
 public class videoFiles_init extends setUP_local {
 
-    @Test(description = "проверка вкадки сеть ")
+    @Test(description = "Видео файлы", groups = "menuSystem")
+    @TestRailId(id = "56838",version = "1")
     public void videoFiles() {
         System.out.println("проверка титла");
         videoFiles_test.videoFiles_title();
