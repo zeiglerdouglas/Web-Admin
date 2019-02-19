1d7cb62b33dedeaa92919fc40e1e0a1ef22fbcb4
A.Krasheninnikov <izrail_izrail@rambler.ru>
2019-02-19 12:49:27 +0300

    добавление id для тестов = 56851

--- src/test/java/leftPanel/menuJurnal/init/inPageLine_init.java
+++ src/test/java/leftPanel/menuJurnal/init/inPageLine_init.java
@@ -1,14 +1,15 @@
 package test.java.leftPanel.menuJurnal.init;
 
+import main.java.annotations.TestRailId;
 import main.java.setUp.setUP_local;
 import org.testng.annotations.Test;
-import test.java.leftPanel.menuJurnal.test.filterCalendar_test;
 import test.java.leftPanel.menuJurnal.test.inPageLine_test;
 
 
 public class inPageLine_init extends setUP_local {
 
-    @Test(description = "проверка журнал - календарь ")
+    @Test(description = "отображение количества строк на страницу", groups = "menuJurnal")
+    @TestRailId(id = "56851",version = "1")
     public void inPageLine() {
         System.out.println("проверка титла");
         inPageLine_test.inPageLine_title();
