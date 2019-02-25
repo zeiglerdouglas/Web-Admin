1630234e252cea2b188bdfe95bdcdd730706b02d
A.Krasheninnikov <izrail_izrail@rambler.ru>
2019-02-25 16:21:00 +0300

    56873 - done - переход по вкладкам после заполнения

--- src/test/java/centralUpPanel/stateUpPanel/stateUpPanel.java
+++ src/test/java/centralUpPanel/stateUpPanel/stateUpPanel.java
@@ -105,7 +105,68 @@ public class stateUpPanel extends setUP_local_oneTest {
         System.out.println("конец теста");
     }
     public static void upSwitchPanel() {
+        System.out.println("определение переменных");
+        SelenideElement global = $(byId("app"));
+        SelenideElement wrapper = global.$(byClassName("app-wrapper"));
+        SelenideElement panelLeft = wrapper.$(byClassName("sidebar-container"));
+        panelLeft.shouldBe(visible.because("находиться слева"));
 
+        System.out.println("собираем в коллекцию элементы");
+        ElementsCollection collLeftPanel = panelLeft.$(byClassName("el-menu")).$$(tagName("a"));
+        collLeftPanel.get(1).shouldBe(visible.because("журналы"));
+        collLeftPanel.get(2).shouldBe(visible.because("консоль"));
+        collLeftPanel.get(3).shouldBe(visible.because("сервис"));
+        collLeftPanel.get(4).shouldBe(visible.because("настройки"));
+        collLeftPanel.get(0).shouldBe(visible.because("система"));
+
+        collLeftPanel.get(1).hover().click();
+        collLeftPanel.get(2).hover().click();
+        collLeftPanel.get(3).hover().click();
+        collLeftPanel.get(4).hover().click();
+        collLeftPanel.get(0).hover().click();
+
+        System.out.println("проверка добавление в панель сверху");
+        System.out.println("определение переменных");
+        SelenideElement mainContainer = wrapper.$(byClassName("main-container"));
+        SelenideElement tagsViewContainer = mainContainer.$(byClassName("tags-view-container"));
+        SelenideElement scrollContainer = tagsViewContainer.$(byClassName("scroll-container"));
+        SelenideElement scrollWrapper = scrollContainer.$(byClassName("scroll-wrapper"));
+        System.out.println(scrollWrapper);
+        ElementsCollection collscrollWrapper = scrollWrapper.$$(tagName("a"));
+        System.out.println(collscrollWrapper);
+
+        System.out.println("проверка на видимость в панеде элементов");
+        collscrollWrapper.get(1).shouldBe(visible.because("Журналы"));
+        collscrollWrapper.get(1).hover();
+        collscrollWrapper.get(2).shouldBe(visible.because("консоль"));
+        collscrollWrapper.get(2).hover();
+        collscrollWrapper.get(3).shouldBe(visible.because("сервис"));
+        collscrollWrapper.get(3).hover();
+        collscrollWrapper.get(4).shouldBe(visible.because("настройки"));
+        collscrollWrapper.get(4).hover();
+        collscrollWrapper.get(0).shouldBe(visible.because("система"));
+        collscrollWrapper.get(0).hover();
+
+        System.out.println("переход по вкладкам после добавления");
+        collscrollWrapper.get(1).shouldBe(visible.because("Журналы"));
+        collscrollWrapper.get(1).click();
+        collscrollWrapper.get(1).findAll(byClassName("router-link-exact-active")).texts();
+        collscrollWrapper.get(2).shouldBe(visible.because("консоль"));
+        collscrollWrapper.get(2).click();
+        collscrollWrapper.get(2).findAll(byClassName("router-link-exact-active")).texts();
+        collscrollWrapper.get(3).shouldBe(visible.because("сервис"));
+        collscrollWrapper.get(3).click();
+        collscrollWrapper.get(3).findAll(byClassName("router-link-exact-active")).texts();
+        collscrollWrapper.get(4).shouldBe(visible.because("настройки"));
+        collscrollWrapper.get(4).click();
+        collscrollWrapper.get(4).findAll(byClassName("router-link-exact-active")).texts();
+        collscrollWrapper.get(0).shouldBe(visible.because("система"));
+        collscrollWrapper.get(0).click();
+        collscrollWrapper.get(0).findAll(byClassName("router-link-exact-active")).texts();
+
+
+
+        System.out.println("конец теста");
 
 
     }
